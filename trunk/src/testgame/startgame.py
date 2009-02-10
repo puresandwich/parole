0c2743ea9abe858f51120471244080fb2e9f2e3b
max.bane <max.bane@28a6635c-ef25-11dd-b507-8b4ecd5210be>
2009-02-10 05:32:08 +0000

    Fleshing out test game

--- trunk/src/testgame/startgame.py
+++ trunk/src/testgame/startgame.py
@@ -4,7 +4,7 @@ import parole
 import main
 
 def start():
-    parole.startup('testgame.cfg', main.updateFunc)
+    parole.startup('testgame.cfg', main.updateFunc, gen=True)
 
 if __name__ == "__main__":
     start()
